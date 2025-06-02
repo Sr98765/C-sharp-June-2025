744f3bd413d689b4b71ca7c0c67ef37c04374704
Sr98765 <sunil2019masters@gmail.com>
2025-06-02 14:00:09 +0000

    coomit 2-6-2025

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class StudentGrade
+{
+    public string? studentName;
+    public int math;
+    public int science;
+    public int english;
+
+    public double GetAverage()
+    {
+        return (math + science + english) / 3.0;
+    }
+
+    public bool HasPassed()
+    {
+        return GetAverage() >= 40;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        StudentGrade sg = new StudentGrade();
+        sg.studentName = "Lina";
+        sg.math = 50;
+        sg.science = 40;
+        sg.english = 30;
+
+        Console.WriteLine($"Average: {sg.GetAverage()}");
+        Console.WriteLine($"Passed: {sg.HasPassed()}");
+    }
+}
